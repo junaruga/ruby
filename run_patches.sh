45aa86b009b4970fb7d62eb885c48c62bccc5f2f
Jun Aruga <jaruga@redhat.com>
2025-01-21 16:42:14 +0100

    Update run_patches.sh.

--- run_patches.sh
+++ run_patches.sh
@@ -14,30 +14,30 @@ set -eux
 # Patch14: rubygems-3.5.17-Remove-the-lock-file-for-binstubs.patch
 
 # %patch 0 -p1
-
-cat ruby-2.3.0-ruby_version.patch | patch -p1
+# cat ruby-2.3.0-ruby_version.patch | patch -p1
 
 # %patch 1 -p1
-
-cat ruby-2.1.0-Prevent-duplicated-paths-when-empty-version-string-i.patch | patch -p1
+# cat ruby-2.1.0-Prevent-duplicated-paths-when-empty-version-string-i.patch | patch -p1
 
 # %patch 2 -p1
-
-cat ruby-2.1.0-Enable-configuration-of-archlibdir.patch | patch -p1
+# cat ruby-2.1.0-Enable-configuration-of-archlibdir.patch | patch -p1
 
 # %patch 3 -p1
-
-cat ruby-2.1.0-always-use-i386.patch | patch -p1
+# cat ruby-2.1.0-always-use-i386.patch | patch -p1
 
 # %patch 4 -p1
-
-cat ruby-2.1.0-custom-rubygems-location.patch | patch -p1
+# cat ruby-2.1.0-custom-rubygems-location.patch | patch -p1
 
 # %patch 6 -p1
 # Hunk
+# cat ruby-2.7.0-Initialize-ABRT-hook.patch | patch -p1
+# git am ruby-2.7.0-Initialize-ABRT-hook.patch
 
 # %patch 9 -p1
+# cat ruby-3.3.0-Disable-syntax-suggest-test-case.patch | patch -p1
+
 # %patch 12 -p1
+# cat ruby-3.4.0-Extract-hardening-CFLAGS-to-a-special-hardenflags-variable.patch | patch -p1
+
 # %patch 13 -p1
 # %patch 14 -p1
-
